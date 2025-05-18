51f4f30fbd09fe6bc6b87eeb8e701aa436eef558
franzadrian <112706342+franzadrian@users.noreply.github.com>
2025-05-18 18:42:06 +0800

    Update auth.guard.ts

--- frontend/src/app/_helpers/auth.guard.ts
+++ frontend/src/app/_helpers/auth.guard.ts
@@ -1,31 +1,28 @@
-// auth.guard.ts
+// error.interceptor.js
 
 import { Injectable } from '@angular/core';
-import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 import { AccountService } from '@app/_services';
 
-@Injectable({ providedIn: 'root' })
-export class AuthGuard {
-    constructor(
-        private router: Router,
-        private accountService: AccountService
-    ) { }
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+    constructor(private accountService: AccountService) {}
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        const account = this.accountService.accountValue;
-        if (account) {
-            // check if route is restricted by role
-            if (route.data['roles'] && !route.data['roles'].includes(account.role)) {
-                // role not authorized so redirect to home page
-                this.router.navigate(['/']);
-                return false;
-            }
-            // authorized so return true
-            return true;
-        }
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(
+            catchError(err => {
+                if ([401, 403].includes(err.status) && this.accountService.accountValue) {
+                    // auto logout if 401 or 403 response returned from api
+                    this.accountService.logout();
+                }
 
-        // not logged in so redirect to login page with the return url
-        this.router.navigate(['/account/login'], { queryParams: { returnUrl: state.url } });
-        return false;
+                const error = (err && err.error && err.error.message) || err.statusText;
+                console.error(err);
+                return throwError(error);
+            })
+        );
     }
 }
