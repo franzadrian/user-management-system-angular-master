d3ae596cb1ee842cf17cfd6b40d7285766591f24
franzadrian <112706342+franzadrian@users.noreply.github.com>
2025-05-18 17:23:36 +0800

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://user-management-system-angular-master.onrender.com'
+    apiUrl: 'user-management-system-angular-master-production-08a9.up.railway.app'
 }
