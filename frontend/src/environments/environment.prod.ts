a5a17d8c18af3415248b1dd2994485635cbf53f7
Alias <alias@email.com>
2025-05-18 16:44:16 +0800

    fix render api

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://user-management-system-angular-master-production.up.railway.app'
+    apiUrl: 'https://user-management-system-angular-master.onrender.com'
 }
